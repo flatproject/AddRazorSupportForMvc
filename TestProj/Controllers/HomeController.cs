e2aa105826f44dacde87396f474500b03aab2662
George Gergianakis <george.gergianakis@valid8.cloud>
2022-07-08 13:24:45 +0100

    first commit

---
+++ TestProj/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using TestProj.Models;
+
+namespace TestProj.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var vm = new MyTestModel
+            {
+                DateTimeOffset = DateTimeOffset.Now,
+                DateTime = DateTime.Now,
+                DateTimeUtc = DateTime.UtcNow
+            };
+
+            return View(vm);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TestProj/Models/MyTestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace TestProj.Models
+{
+    public class MyTestModel
+    {
+        public DateTimeOffset DateTimeOffset { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public DateTime DateTimeUtc { get; set; }
+    }
+}
